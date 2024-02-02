fa3633ef2c981b87c256cd1ac7fc156f90ff8136
Khochawongwat <khochawongwat.kongpana@gmail.com>
2024-02-02 13:37:00 +0700

    Refactor model architecture and add new FusedMBConvBlock class

--- model.py
+++ model.py
@@ -15,6 +15,7 @@ class ConvBlock(nn.Module):
             return ProxyNormalization(bn, activation_fn = self.silu, eps = 0.03, n_samples = min(x.shape[0], 256), apply_activation = True).forward()
         else:
             return self.silu(bn)
+
 class MBConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, expand_ratio, include_se = True, proxy_norm = True, group_size = 16, se_ratio = 0.25):
         super().__init__()
@@ -42,7 +43,7 @@ class MBConvBlock(nn.Module):
         if self.expand_ratio != 1:
             self.expand_conv = ConvBlock(in_channels, expansion_channels, self.group_size,  1, 1, activation_fn = True, bias=False)
             self.bn0 = nn.BatchNorm2d(expansion_channels, momentum=self.momentum, eps=self.eps)
-
+            
         #Squeeze and Excitation
         if include_se:
             n_squeezed_channels = max(1, int(in_channels * se_ratio))
@@ -50,7 +51,7 @@ class MBConvBlock(nn.Module):
             self.se_expand = ConvBlock(n_squeezed_channels, out_channels, self.group_size, 1, 1, activation_fn = True)
         
         #Depthwise Convolution
-            self.depthwise_conv = ConvBlock(expansion_channels, expansion_channels, self.group_size, kernel_size, stride, activation_fn = True, bias = True)
+        self.depthwise_conv = ConvBlock(expansion_channels, expansion_channels, self.group_size, kernel_size, stride, activation_fn = True, bias = True)
 
         #Batch Normalization
         self.bn1 = nn.BatchNorm2d(expansion_channels, momentum=self.momentum, eps=self.eps)
@@ -110,6 +111,103 @@ class MBConvBlock(nn.Module):
 
         return x
 
+class FusedMBConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, expand_ratio, include_se = True, proxy_norm = True, group_size = 16, se_ratio = 0.0):
+        super().__init__()
+        self.expand_channels = in_channels * expand_ratio
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.expand_ratio = expand_ratio
+        self.include_se = include_se
+        
+        self.group_size = group_size
+        self.se_ratio = se_ratio
+        self.momentum = 0.9
+        self.include_pn = proxy_norm
+        self.pn = ProxyNormalization(activation_fn = Swish(), eps = 1e-3, apply_activation = True)
+        self.activation_func = Swish()
+
+        #Expansion
+        if expand_ratio != 1:
+            self.expansion_conv = ConvBlock(self.in_channels, self.expand_channels, group_size, 1, 1, activation_fn = True, bias = False)
+            self.bn0 = nn.BatchNorm2d(self.expand_channels, momentum=self.momentum, eps=1e-3)
+        
+        #Squeeze and Excitation
+        if include_se:
+            squeezed_channels = max(1, int(in_channels * se_ratio))
+            self.se_reduce = ConvBlock(self.expand_channels, squeezed_channels, group_size, 1, 1, activation_fn = True)
+            self.se_expand = ConvBlock(squeezed_channels, out_channels, group_size, 1, 1, activation_fn = True)
+        
+        #Projection
+        self.bn2 = nn.BatchNorm2d(out_channels, momentum=self.momentum, eps=1e-3)
+        if(self.expand_ratio != 1):
+            self.project_conv = ConvBlock(self.expand_channels, out_channels, group_size, self.kernel_size, self.stride, activation_fn = False, bias = False)
+        else:
+            self.project_conv = ConvBlock(self.in_channels, out_channels, group_size, 1, 1, activation_fn = False, bias = False)
+            
+    def forward(self, x, drop_connect_rate = None):
+
+        if drop_connect_rate and (drop_connect_rate > 1 or drop_connect_rate < 0):
+            raise ValueError("drop_connect_rate must be between 0 and 1.")
+        
+        #Reserve Identity Block
+        identity_block = x
+
+        #Expansion
+        if self.expand_ratio != 1:
+            bn0 = self.bn0(self.expansion_conv(x))
+            if self.include_pn:
+                x = self.pn.forward(bn0)
+            else:
+                x = self.activation_func(bn0)
+        
+        #Squeeze and Excitation
+        if self.include_se:
+            squeezed_x = nn.AdaptiveAvgPool1d(x, 1)
+            squeezed_x = self.se_expand(self.activation_func(self.se_reduce(squeezed_x)))
+
+            #Activate the squeezed_x for output range of 0 to 1
+            x = torch.sigmoid(squeezed_x) * x
+
+        #Projection
+        bn2 = self.bn2(self.project_conv(x))
+        if self.include_pn:
+            x = self.pn.forward(bn2)
+        else:
+            x = self.activation_func(bn2)
+
+        # Add the identity block to the output of the projection if the input
+        # and output channels are the same and the stride is 1
+            
+        if self.in_channels == self.out_channels and self.stride == 1:
+            if drop_connect_rate:
+                x = x + drop_connect(x, drop_connect_rate, training = self.mode == "train")
+            x += identity_block
+
+        return x
+class Classifier(nn.Module):
+    def __init__(self, in_channels, num_classes, pool = "avg"):
+        super().__init__()
+        self.num_classes = num_classes
+        self.pool = pool
+
+        #Create the pooling layer
+        if pool == "avg":
+            self.pooling = nn.AdaptiveAvgPool2d((1,1))
+        else:
+            self.pooling = nn.AdaptiveMaxPool2d((1,1))
+
+        #Create the fully connected layer
+        self.fc = nn.Linear(in_channels, num_classes)
+    
+    def forward(self, x):
+        x = self.pooling(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
 class EfficientNetNano(nn.Module):
     def __init__(self, width_mult, depth_mult, num_classes, drop_connect_rate, dropout_rate, pool = "avg", group_size = 16):
         super().__init__()
@@ -140,10 +238,11 @@ class EfficientNetNano(nn.Module):
         #Create the head block
         in_channels = round_filters(self.block_params[-1][5], width_mult)
         out_channels = 1280
+
         self.head = nn.Sequential([
-            ConvBlock(in_channels, out_channels, self.group_size, 1, 1, activation_fn = True, proxy_norm = True),
+            ConvBlock(in_channels, out_channels, 1, 1, 1, activation_fn = True, proxy_norm = True),
             nn.BatchNorm2d(num_features=out_channels, momentum=self.momentum, eps= self.eps),
-            ProxyNormalization(activation_fn = Swish(), eps = 0.03, n_samples =  min(out_channels, 256), apply_activation = False),
+            ProxyNormalization(activation_fn = Swish(), eps = 0.03, apply_activation = False),
             Swish(), #Activation function for the head using Swish instead of ReLU
         ])
 
@@ -151,7 +250,7 @@ class EfficientNetNano(nn.Module):
         self.stem = nn.Sequential([
             ConvBlock(3, out_channels, self.group_size, 3, 2, activation_fn = True, proxy_norm = True),
             nn.BatchNorm2d(num_features=out_channels, momentum=self.momentum, eps= self.eps),
-            ProxyNormalization(activation_fn = Swish(), eps = 0.03, n_samples =  min(out_channels, 256), apply_activation = False),
+            ProxyNormalization(activation_fn = Swish(), eps = self.eps, apply_activation = False),
             Swish(), #Activation function for the stem using Swish instead of ReLU
         ])
 
@@ -169,14 +268,8 @@ class EfficientNetNano(nn.Module):
         else:
             self.dropout = nn.Identity()
         
-        #Create the pooling layer
-        if pool == "avg":
-            self.pooling = nn.AdaptiveAvgPool2d((1,1))
-        else:
-            self.pooling = nn.AdaptiveMaxPool2d((1,1))
-
-        self.fc = nn.Linear(out_channels, num_classes)
-
+        #Create the classifier
+        self.classifier = Classifier(in_channels, num_classes, pool = pool)
         ##Initialize the weights
 
     def stage_block(self, params, width_mult, depth_mult, i):
